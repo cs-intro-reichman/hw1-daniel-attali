9b86b3832a66b063ddc3a2738a98c275248c2abe
Daniel ATTALI <danielattali@MacBook-Pro-de-Daniel.local>
2025-10-31 20:12:09 +0200

    finished

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,22 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	int a,b,c ;
+	a= (int)(Math.random()*10)+1 ;
+	b= (int)(Math.random()*10)+1 ;
+	c= (int)(Math.random()*10)+1 ;
+	if (a>b && b>c) { 
+	System.out.println(c+","+b+","+a); 
+	}else if (a<=b && b<=c) {
+	System.out.println(a+","+b+","+c);	
+	}else if (a<=c && b>=c) {
+	System.out.println(a+","+c+","+b);}
+	else if (a>=c && b<=c) {
+	System.out.println(b+","+c+","+a);
+	}else if (a>=c && b>=a) {
+	System.out.println(c+","+a+","+b);}
+	else if (a<=c && b<=a) {
+	System.out.println(b+","+a+","+c);}
 	}
 }
+
