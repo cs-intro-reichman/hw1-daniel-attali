eab2678e695396216329d5135126109252fe4d5d
Daniel ATTALI <danielattali@MacBook-Pro-de-Daniel.local>
2025-11-04 21:55:29 +0200

    fixed syntax issue

--- Bill3.java
+++ Bill3.java
@@ -1,12 +1,15 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-		String name2 = args[1];
-		String name3 = args[2];
-		double bill  = Double.parseDouble(args[3])/3.0;
-	    bill = Math.round(bill*10.0)/10.0;
-		System.out.println("dear "+name3+", "+name2+" and "+name1+": pay "+bill+" Shekels each");
-	}
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+
+        double total = Double.parseDouble(args[3]);
+        double bill = total / 3.0;
+
+        bill = Math.ceil(bill * 10.0) / 10.0;
+
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
+                           ": pay " + String.format("%.1f", bill) + " Shekels each");
+    }
 }
