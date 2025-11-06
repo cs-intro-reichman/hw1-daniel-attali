d842ea0cc493b08c84da127435546f17a08286be
Daniel ATTALI <danielattali@MacBook-Pro-de-Daniel.local>
2025-11-06 10:19:39 +0200

    fixed style issue

--- Bill3.java
+++ Bill3.java
@@ -9,6 +9,6 @@ public class Bill3 {
 
         bill = Math.ceil(bill);
 
-        System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 +": pay " + bill + " Shekels each");
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +": pay " + bill + " Shekels each");
     }
 }
